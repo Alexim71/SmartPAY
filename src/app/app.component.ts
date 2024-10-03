c566e8509db00f418691c185f7509d67bf0acd29
ALEXIMA Sanlove Eden <sanloveedena@gmail.com>
2024-10-03 15:49:48 -0400

    with deep-link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone  } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +9,30 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private menu: MenuController) {}
+  constructor(private menu: MenuController, private router: Router, private zone: NgZone) {}
 
   openPage(page: string) {
     console.log('Navigating to page:', page);
     // Ajouter la logique pour la navigation de page
+    this.initializeApp();
   }
 
   closeMenu() {
     this.menu.close();
   }
+
+  initializeApp() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+        this.zone.run(() => {
+            // Example url: https://beerswift.app/tabs/tab2
+            // slug = /tabs/tab2
+            const slug = event.url.split(".app").pop();
+            if (slug) {
+                this.router.navigateByUrl(slug);
+            }
+            // If no match, do nothing - let regular routing
+            // logic take over
+        });
+    });
+}
 }
